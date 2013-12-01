7168ea6a50df928419020707110d6d4684b85323
Baptiste <baptiste@baptiste-N61Vg.(none)>
2013-12-01 01:34:49 +0100

    amelioration de l'interface, preparation de la mise en place du chat

--- views/perso/js/scriptInterface.js
+++ views/perso/js/scriptInterface.js
@@ -6,9 +6,10 @@ var status = "online";
         $( "#chatPanel" ).empty();
         client.forEach(function(val,idx) {
           if (val.pseudo != PSEUDO) {
-            $("#chatPanel").append("<div class='row chatPanelRow'><div id=AUTO"+val.pseudo+" class='glyphicon glyphicon-user "+val.status+"'></div><div class='chatPanelDiv'>  "+val.pseudo+"</div></div>");
+            $("#chatPanel").append("<div class='row chatPanelRow cursorPointer' onClick=\"addDiscussion('"+val.pseudo+"')\"><div id=AUTO"+val.pseudo+" class='glyphicon glyphicon-user "+val.status+" cursorPointer'></div><div class='chatPanelDiv cursorPointer'>  "+val.pseudo+"</div></div>");
           }
         });
+        updateIHMDiscussion();
       });
       
       // client deconnection
@@ -16,13 +17,15 @@ var status = "online";
         $( "#chatPanel" ).empty();
         client.forEach(function(val,idx) {
           if (val.pseudo != PSEUDO) {
-            $("#chatPanel").append("<div class='row chatPanelRow'><div id=AUTO"+val.pseudo+" class='glyphicon glyphicon-user "+val.status+"'></div><div class='chatPanelDiv'>"+val.pseudo+"</div></div>");
+            $("#chatPanel").append("<div class='row chatPanelRow cursorPointer' onClick=\"addDiscussion('"+val.pseudo+"')\"><div id=AUTO"+val.pseudo+" class='glyphicon glyphicon-user "+val.status+" cursorPointer'></div><div class='chatPanelDiv cursorPointer'>"+val.pseudo+"</div></div>");
           }
         });
+        updateIHMDiscussion();
       });
 
       socket.on('status', function(data) {
         $("#AUTO"+data.pseudo).removeClass("absent busy online").addClass(data.status);
+        updateIHMDiscussion();
       });
 
       function changeStatus() {
@@ -40,3 +43,93 @@ var status = "online";
           socket.emit('statusChanged', "online");
         }
       }
+
+      var discussion = [];
+      var scroll = 0;
+
+      function indexOfDiscussion(pseudo) {
+        var index = -1;
+        discussion.forEach(function(val, idx) {
+          if (val.pseudo === pseudo) {
+            index=idx;
+          }
+        });
+        return index;
+      }
+
+      function addDiscussion(pseudo) {
+        if (indexOfDiscussion(pseudo) == -1) {
+          discussion.push({pseudo:pseudo, message:[], collapse:false, unReadMessage:false});
+        }
+        else {
+          toggleCollapseDiscussion(pseudo);
+        }
+        updateIHMDiscussion();
+      }
+
+      function removeDiscussion(pseudo) {
+        var index = indexOfDiscussion(pseudo);
+        if (index !== -1) {
+          discussion.splice(index, 1);
+        }
+        updateIHMDiscussion();
+      }
+
+      function toggleCollapseDiscussion(pseudo) {
+        var index = indexOfDiscussion(pseudo);
+        discussion[index].collapse ? discussion[index].collapse = false : discussion[index].collapse = true;
+        updateIHMDiscussion();
+      }
+
+      function updateIHMDiscussion() {
+        $(".chatFen").remove();
+        $(".chatFenCollapse").remove();
+        console.log(discussion);
+        if (discussion.length < 5) {
+          scroll = 0;
+          var chatWindow;
+          var classe;
+          discussion.forEach(function(val, idx) {
+            chatWindow = "";
+            if ($("#AUTO"+val.pseudo).length > 0) {
+              if ($("#AUTO"+val.pseudo).is(".busy")) {
+                classe = "warning";
+              } else if ($("#AUTO"+val.pseudo).is(".absent")) {
+                classe = "danger";
+              } else {
+                classe = "success";
+              }
+            }
+            else {
+              classe = "";
+            }
+
+            if (!val.collapse) {
+              chatWindow += "<div class='panel panel-default chatFen discussion"+(idx+1)+"' id='AUTO_CHAT_'"+val.pseudo+">";
+              chatWindow += "<div class='panel-heading'>";
+              chatWindow += "<h2 class='panel-title text-center text-"+classe+"' style='font-weight : bold;'>"+val.pseudo+"</h2>";
+              chatWindow += "<p class='glyphicon glyphicon-chevron-down baisserChatFen' onClick='toggleCollapseDiscussion(\""+val.pseudo+"\")'></p>";
+              chatWindow += "<p class='glyphicon glyphicon-remove fermerChatFen' onClick='removeDiscussion(\""+val.pseudo+"\")'></p> </div>";
+              chatWindow += "<div class='panel-body'>";
+              chatWindow += "<div class=\"contentChatWindow\"></div>";
+              chatWindow += "<input class=\"inputChatWindow\"type=\"text\" onKeydown=\"ListenKeyPressed();\"></input>";
+              chatWindow += "</div> </div>";
+            }
+            else {
+              chatWindow += "<div class='alert alert-"+classe+" chatFenCollapse discussion"+(idx+1)+"' id='AUTO_CHAT_'"+val.pseudo+">";
+              chatWindow += "<h2 class='panel-title text-center text-"+classe+"' style='font-weight : bold;'>"+val.pseudo+"</h2>";
+              chatWindow += "<p class='glyphicon glyphicon-chevron-up baisserChatFen' onClick='toggleCollapseDiscussion(\""+val.pseudo+"\")'></p>";
+              chatWindow += "<p class='glyphicon glyphicon-remove fermerChatFen' '></p> </div>";
+              chatWindow += "</div>";
+            }
+            $("#pageBody").append(chatWindow);
+          });
+        }
+      }
+      //onClick='removeDiscussion(\""+val.pseudo+"\")
+      /*
+      function ListenKeyPressed () {
+        if(event.keyCode==13) {
+          console.log("BUMP!!!");
+        }
+      }*/
